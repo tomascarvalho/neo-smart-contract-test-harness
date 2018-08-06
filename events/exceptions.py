bba56bb5b130cf925d622167bc650adea7f1b0de
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2018-08-06 14:43:22 +0100

    Add custom exceptions and __init__.py

---
+++ events/exceptions.py
@@ -0,0 +1,15 @@
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class NodeBlockingException(Error):
+    """Exception raised when a node is blocking.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
