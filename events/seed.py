278d6cd92fec5ad15e096bbad93fe25d9f1a16bd
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2019-01-18 00:54:09 +0000

    Remove seeds file

--- events/seed.py
+++
@@ -1,6 +0,0 @@
-from models import Session, TestCase
-
-session = Session()
-test_case = TestCase(contract_hash='f3da12622e9bb2b3f367a650a81fd8c70b2eb495', event_type='SmartContract.Runtime.Log', expected_payload_type='String', expected_payload_value='Contract was called')
-session.add(test_case)
-session.commit()
